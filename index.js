d9d7fff6059c07149dd31a61b595f2c4a006f173
Manishrajwar <manishsinghrajwar80@gmail.com>
2025-02-02 14:33:50 +0530

    new

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import express from "express"
 import serverconfig from "./config/serverconfig.js"
 import monitorrouter from "./router/monitorrouter.js"
+import socketIo from "socket.io"; 
+import cors from "cors";  // Import the CORS package
+
 
 const app = express();
+const server = http.createServer(app);  // Create an HTTP server using express
+const io = socketIo(server);  // Initialize Socket.IO with the HTTP server
+
+app.use(cors());
 
 app.use(express.static("public"));
 
@@ -16,7 +23,15 @@ const PORT = serverconfig.PORT || 5000;
 // routes 
 app.use("/" ,monitorrouter );
 
+
+// Real-time monitoring: emit server metrics every 5 seconds
+setInterval(async () => {
+    const metrics = await monitorservice.getMetrics();
+    io.emit("server_metrics", metrics);  // Emit metrics to all connected clients
+  }, 5000);
+
 app.listen(PORT , ()=>{
     console.info(`server is running on port ${PORT}`)
 })
+
 
