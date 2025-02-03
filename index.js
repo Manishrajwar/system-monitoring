a1c36229471d24dd4c7dc75e6a4935050ed2e7eb
Manishrajwar <manishsinghrajwar80@gmail.com>
2025-02-03 07:58:52 +0530

    don

--- index.js
+++ index.js
@@ -25,16 +25,10 @@ app.use(express.static("public"));
 app.use(express.json()); 
 
 
-mongoose.connect('mongodb://localhost:27017/serverMetrics', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-    .then(() => {
-      console.log('Connected to MongoDB');
-    })
-    .catch(err => {
-      console.error('MongoDB connection error:', err);
-    });
+mongoose.connect('mongodb://127.0.0.1:27017/serverMetrics')  // Removed deprecated options
+  .then(() => console.log('✅ Connected to MongoDB'))
+  .catch(err => console.error('❌ MongoDB connection error:', err));
+
 
 app.set("view engine", "ejs");
 
